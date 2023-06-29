cd913585fef3c6be71c16ab5f7080ec1089a8cd3
clay.zenx <clayzenx@gmail.com>
2023-06-29 15:19:48 +0300

    remove error

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  configureWebpack: {
+    devServer: {
+      client: {
+        overlay: {
+          runtimeErrors: (error) => {
+            if (error.message === "ResizeObserver loop limit exceeded") {
+              return false;
+            }
+            return true;
+          },
+        }
+      }
+    }
+  }
 })
